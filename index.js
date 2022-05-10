c50c49c8df8623e254bba7cea2093341d769d903
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-10 10:19:29 +0200

    Ejemplo de como se ve en pantalla un string, number, boolean, undefined, object, array y function.

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-let nombre = 'Pedro';
-console.log('nombre', nombre)
+let soyUnNull = null
+console.log('null', null)
+
+let soyUnUndefined = undefined
+console.log('undefined', soyUnUndefined)
+
+let soyUnBooleano = false
+console.log('booleano', soyUnBooleano)
+
+let soyUnNumero = 2
+console.log('numero', soyUnNumero)
+
+let soyUnString = 'HOla'
+console.log('string', soyUnString)
+
+let soyUnArray = ['ageOfEmpires', 'Civilization', 'HeartsOfIron']
+console.log('array', soyUnArray)
+
+let soyUnObject = {
+    stringDentroDeUnObject: 'Pedro',
+    numberDentroDeUnObject: 38,
+    booleanoDentroDeUnObject: true,
+    objectDentroDeUnObject: {
+        ciudad: 'Terrassa',
+        intereses: [
+            'leer', 'cine', 'viajar', {nombre: 'Luis'}, [1,2,3 ], soyUnString
+        ]
+    }
+}
+console.log('object', soyUnObject)
+
+function loguearNombre(soyUnString){
+    console.log('loguear', soyUnString)
+}
+console.log('funcion', funcion)
