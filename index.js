531fdd346c6a5dc76905b4a094323a38929257ac
Mov1ux <javimourelo@hotmail.com>
2022-05-10 11:26:08 +0200

    javascript etiquetas

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ console.log("booleano", soyElBooleanoDeJavi)
 
 let soyElUndefineDeJavi = undefined
 console.log("undefined", soyElUndefineDeJavi)
+
+let soyElStringDeJavi = "Javi"
+console.log("string", soyElStringDeJavi)
+
+let soyElObjectDeJavi ={
+    Nombre: "Javi",
+    Apellido: "Garcia",
+}
+console.log("objecto", soyElObjectDeJavi)
+
+let soyElArrayDeJavi = ["tengo", "mucha", "Hambre"]
+console.log("Array", soyElArrayDeJavi)
+
+let soyElNumeroDeJavi = 22
+console.log("numero", soyElNumeroDeJavi)
+
+function soyLaFuncionDeJavi (soyElStringDeJavi){
+        console.log("funcion", soyElStringDeJavi)
+}
+console.log("funcion", soyLaFuncionDeJavi) 
