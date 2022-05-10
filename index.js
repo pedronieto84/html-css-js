733b81c5cd00a848a4ca6e51db7a46db6663faed
Mov1ux <javimourelo@hotmail.com>
2022-05-10 12:54:55 +0200

    Funcion edades

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-let soyElNullDeJavi = null 
-console.log("null", soyElNullDeJavi)
-
-let soyElBooleanoDeJavi = false
-console.log("booleano", soyElBooleanoDeJavi) 
-
-let soyElUndefineDeJavi = undefined
-console.log("undefined", soyElUndefineDeJavi)
-
-let soyElStringDeJavi = "Javi"
-console.log("string", soyElStringDeJavi)
-
-let soyElObjectDeJavi ={
-    Nombre: "Javi",
-    Apellido: "Garcia",
-}
-console.log("objecto", soyElObjectDeJavi)
-
-let soyElArrayDeJavi = ["tengo", "mucha", "Hambre"]
-console.log("Array", soyElArrayDeJavi)
-
-let soyElNumeroDeJavi = 22
-console.log("numero", soyElNumeroDeJavi)
-
-function soyLaFuncionDeJavi (soyElStringDeJavi){
-        console.log("funcion", soyElStringDeJavi)
-}
-console.log("funcion", soyLaFuncionDeJavi) 
+function mayoriaDeEdad (edad){
+    if (edad <= 18) {
+        console.log("es menor de edad")
+    } else if( edad >=65) {
+            console.log("es jubilado")
+    } else{
+        console.log("estas en  edad de trabajar")}
+    }
+mayoriaDeEdad ()
