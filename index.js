f215321f723b780d2f104c05056e1dcf28b20aaf
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-10 12:11:39 +0200

    Simbolo de diferente.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// El signo de doble igual == . EVALUAR o COMPARAR
-
-// El parentesiss sirve para agrupar estructuras de logica.
-const esCierto =  ( 1 == '1' ) 
-console.log("es cierto", esCierto);
-
-const esCiertoTripleIgual = ( 1 === '1' )
+// El signo de doble igual != . EVALUAR o COMPARAR lo contrario
 
 
+const esDiferente =  ( 1 == '1' ) 
+console.log("es diferente?", esDiferente);
+
+const esCiertoTripleIgual = ( 1 !== '1' )
 // Simbolo de triple Igual es más estricto y me va a evaluar no solo el dato sino el tipo de dato
-console.log("es cierto triple igual", esCiertoTripleIgual);
+console.log("es diferente? triple comparador", esCiertoTripleIgual);
 
 
