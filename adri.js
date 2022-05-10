2ca40c0a540cde80e066886d030194be4a6fc72b
Adrix8 <adrianperezpascual@gmail.com>
2022-05-10 10:57:09 +0200

    variables adri

--- adri.js
+++ adri.js
@@ -6,3 +6,18 @@ console.log('undefined', holaSoyUnUndefined);
 
 let holaSoyUnBooleano = false
 console.log('Boleano', holaSoyUnBooleano );
+
+let soyUnString = "Hola mis panas"
+console.log("String", soyUnString);
+
+let soyUnObjeto = {
+    nombre : "Adrian",
+    apellido : "Perez"
+};
+console.log("Obejeto",soyUnObjeto);
+
+let soyUnArray = [ "rocketleague", "pokemon", "leagueoflegends"]
+console.log("array",soyUnArray);
+
+let number = 8
+console.log("numero",number);
