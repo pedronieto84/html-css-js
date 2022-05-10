6a36732dab475c38190f30498305773438f78808
VicJGR <vjgomezrios@gmail.com>
2022-05-10 10:35:13 +0200

    Ejemplo clases de multiples valores VJ

---
+++ Victor.js
@@ -0,0 +1,8 @@
+let elNullDeVictor = null
+console.log("null", elNullDeVictor)
+
+let elUndefinedDeVictor = undefined
+console.log("undefined", elUndefinedDeVictor)
+
+let soyElBooleanoDeVictor = false
+console.log("false", soyElBooleanoDeVictor)
