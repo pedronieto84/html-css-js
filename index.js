e77be6839acf75173200e759a068bbafc57eedc5
Maru <marianalopezeikilis>
2022-05-10 10:53:58 +0200

    todas las etiquetas en console

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ console.log('undefined', soyElUndefinedDeMariana)
 let soyElBooleanDeMariana = false
 console.log('booleano',soyElBooleanDeMariana)
 
+let soyElNumberDeMariana = 3
+console.log ('numero', soyElNumberDeMariana)
+
+let soyElStringDeMariana = 'String de Mariana'
+console.log('string', soyElStringDeMariana)
+
+let soyElObjectDeMariana = 
+{
+    nombre: 'Mariana',
+    apellido: 'Lopez',
+
+}
+console.log('objeto', soyElObjectDeMariana)
+
+let soyElArrayDeMariana= ['buble','saboteur','truco']
+console.log('array',soyElArrayDeMariana)
+
+function soyLaFuncionDeMariana (soyElStringDeMariana) {
+    console.log ('funcion',soyElStringDeMariana)
+}
