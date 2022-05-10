a211c32de55882b01fa5d1551081bdc511edffc0
AxelWine <contacto.axelwine@gmail.com>
2022-05-10 10:28:04 +0200

    He creado el index.js y he añadido un null, un booleano y un undefined.

---
+++ index.js
@@ -0,0 +1,8 @@
+let soyUnNullDeAlex = null;
+console.log(soyUnNullDeAlex);
+
+let soyUnBooleanoDeAlex = true;
+console.log(soyUnBooleanoDeAlex);
+
+let soyUnUndefinedDeAlex = undefined;
+console.log(soyUnUndefinedDeAlex);
