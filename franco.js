b2b5390ef2b8c17845af88a3629abda702843bf9
FrancOlano <olanofranco1@gmail.com>
2022-05-10 11:21:51 +0200

    string objct array fuction y number de franco

--- franco.js
+++ franco.js
@@ -1,8 +1,30 @@
 let soyElNullDeFranco = null
-console.log(soyElNullDeFranco)
+console.log('null', soyElNullDeFranco)
 
 let soyElBooleanoDeFranco = true
-console.log(soyElBooleanoDeFranco)
+console.log('booleano',soyElBooleanoDeFranco)
 
 let soyElUndefinedDeFranco = undefined
-console.log(soyElUndefinedDeFranco)
+console.log('undefined',soyElUndefinedDeFranco)
+
+let soyElStringDeFranco = 'Hola'
+console.log('string',soyElStringDeFranco)
+
+let soyElObjectDeFranco ={
+    nombre: 'Franco',
+    apellido: 'Olano',
+}
+console.log('objeto',soyElObjectDeFranco)
+
+let soyElArrayDeFranco = ['overwatch', 'GTA', 'CS:GO']
+console.log('array',soyElArrayDeFranco)
+
+function funcionDeFranco(){
+    nombre='Franco'
+    console.log('funcion',nombre)
+}
+funcionDeFranco() 
+
+
+let soyElNumeroDeFranco = 579
+console.log('numero',soyElNumeroDeFranco)
