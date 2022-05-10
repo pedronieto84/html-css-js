2c80efa0b8559548e301e3d51b9cd4330f72294d
vazquezpmarcos <vazquezpmarcos@gmail.com>
2022-05-10 10:34:09 +0200

    indexjs

---
+++ index.js
@@ -0,0 +1,42 @@
+let nullDeMarcos = null
+console.log('null de Marcos', nullDeMarcos)
+
+let undefinedDeMarcos = undefined
+console.log('undefined de Marcos', undefinedDeMarcos)
+
+let booleanoDeMarcos = false
+console.log('booleano de Marcos', booleanoDeMarcos)
+
+let usuarioTinder = {
+    nombre: "Marcos",
+    edad: 21,
+    activo: true,
+    email: "emailgenerico@gmail.com"
+}
+
+// let soyUnNumero = 2
+// console.log('numero', soyUnNumero)
+
+// let soyUnString = 'HOla'
+// console.log('string', soyUnString)
+
+// let soyUnArray = ['ageOfEmpires', 'Civilization', 'HeartsOfIron']
+// console.log('array', soyUnArray)
+
+// let soyUnObject = {
+//     stringDentroDeUnObject: 'Pedro',
+//     numberDentroDeUnObject: 38,
+//     booleanoDentroDeUnObject: true,
+//     objectDentroDeUnObject: {
+//         ciudad: 'Terrassa',
+//         intereses: [
+//             'leer', 'cine', 'viajar', {nombre: 'Luis'}, [1,2,3 ], soyUnString
+//         ]
+//     }
+// }
+// console.log('object', soyUnObject)
+
+// function loguearNombre(soyUnString){
+//     console.log('loguear', soyUnString)
+// }
+// console.log('funcion', loguearNombre)
