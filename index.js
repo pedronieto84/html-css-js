b9c4488becc6476bc5d0e081de7e94c33def1b9e
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-10 13:37:35 +0200

    Ejemplo de como trabajar solamente con ifs y returns.

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-// Bucle for que se va a ejecutar tantas veces como determine en la constante personas
+// 
 
-const personas = 20;
+function mayoriaDeEdad( edad){
+    if (edad <= 18) {
+        console.log("menor de edad ");
+        return;
+    }
 
-// index += 2 es una abreviacion de
-// index = index + 2
+    if(edad >= 65){
+        console.log('jubilado')
+        return
+    }
 
-for (let index = 0; index <= personas; index += 4) {
-  console.log("index", index);
 }
 
-// Si no le pongo ningún número interpreta que es un 1.
+mayoriaDeEdad(19)
 
-for (let index = 0; index <= personas; index ++) {
-  console.log("index", index);
-}
+
+
+
+
+
+
+
 
