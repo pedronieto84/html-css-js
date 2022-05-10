2ce9deea4251b9c582092ed36b35fa591912b50b
marcelhannouti <mel@elpuig.xeill.net>
2022-05-10 12:57:20 +0200

    ejercicio javascript

---
+++ indice.js
@@ -0,0 +1,21 @@
+function mayoredad (edad){
+if(edad >= 65){
+console.log(`es mayor de 65`)
+}else if(edad >=19 ){
+    console.log(`es mayor de 19`)
+}else{
+    console.log(`es menor `)
+}
+}
+mayoredad(30)
+
+function mayoredad2 (edad){
+if(edad >=19 && edad <= 65){
+    console.log(`es mayor de 19`)
+    }else if(edad >=65 ){
+        console.log(`es mayor de 65`)
+    }else{
+        console.log(`es menor `)
+    }
+    }
+    mayoredad2(30)
