52e19a2055030349c47faede27601551046d2af6
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-10 13:21:37 +0200

    Simbolo += y simbolo ++

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 // Bucle for que se va a ejecutar tantas veces como determine en la constante personas
 
+const personas = 20;
 
-const personas = 20
+// index += 2 es una abreviacion de
+// index = index + 2
 
-for (let index = 0;    index <= personas;      index ++) {
-    console.log('index', index)
+for (let index = 0; index <= personas; index += 4) {
+  console.log("index", index);
 }
 
+// Si no le pongo ningún número interpreta que es un 1.
 
-
-
-
-
-
-
-
-
-
+for (let index = 0; index <= personas; index ++) {
+  console.log("index", index);
+}
 
