715b3e427ea989df660b27c452a7e1f1b3509cc6
Adrix8 <adrianperezpascual@gmail.com>
2022-05-10 12:57:28 +0200

    if adri

--- adri.js
+++ adri.js
@@ -1,23 +1,8 @@
-let holaSoyUnNull = null
-console.log('null', holaSoyUnNull);
-
-let holaSoyUnUndefined = undefined
-console.log('undefined', holaSoyUnUndefined);
-
-let holaSoyUnBooleano = false
-console.log('Boleano', holaSoyUnBooleano );
-
-let soyUnString = "Hola mis panas"
-console.log("String", soyUnString);
-
-let soyUnObjeto = {
-    nombre : "Adrian",
-    apellido : "Perez"
-};
-console.log("Obejeto",soyUnObjeto);
-
-let soyUnArray = [ "rocketleague", "pokemon", "leagueoflegends"]
-console.log("array",soyUnArray);
-
-let number = 8
-console.log("numero",number);
+edad = 70
+if (edad <= 18){
+    console.log("eres menor de edad")
+}else if (edad >= 65){
+    console.log("abuelo")
+}else{
+    console.log("edad para trabajar")
+}
