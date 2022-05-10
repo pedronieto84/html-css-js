11bd2af9f9a3cea0da6f74822da87e6608b23415
marcelhannouti <mel@elpuig.xeill.net>
2022-05-10 10:33:52 +0200

    ejemplo js

---
+++ index.js
@@ -0,0 +1,8 @@
+let soyelNullmarc = null
+console.log(`null`,soyelNullmarc)
+
+let soyelbooleanomarc = true 
+console.log(`boolean`,soyelbooleanomarc)
+
+let soyelundefinedmarc
+console.log(`undifiend`,soyelundefinedmarc)
