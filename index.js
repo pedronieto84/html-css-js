1711d053e773832ec323810cdd4021f65de705de
Maru <marianalopezeikilis>
2022-05-10 10:28:50 +0200

    ejemplo de console

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+let soyElNullDeMariana = null
+console.log('null', soyElNulldeMariana)
+
+let soyElUndefinedDeMariana = undefined
+console.log('undefined', soyElUndefinedDeMariana)
+
+let soyElBooleanDeMariana = false
+console.log('booleano',soyElBooleanDeMariana)
+
