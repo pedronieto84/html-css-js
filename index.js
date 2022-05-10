e525b1d7a230ce97ab8e09a98a84305a3ed3a3ac
victoriaschwin <vicky.schwint95@gmail.com>
2022-05-10 11:23:53 +0200

    string, objec, array, number y funcion

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 let soyElNullDeVictoria = null
-console.log('null', soyElNullDeVictoria)
+    console.log('null', soyElNullDeVictoria)
 
 let soyUnUndefinedDeVictoria = undefined
-console.log('undefined', soyUnUndefinedDeVictoria)
+    console.log('undefined', soyUnUndefinedDeVictoria)
 
 let soyUnBooleanoDeVictoria = false
-console.log('booleano', soyUnBooleanoDeVictoria)
+    console.log('booleano', soyUnBooleanoDeVictoria)
+
+let soyUnStringDeVictoria = 'Buenos Días'
+    console.log('string', soyUnStringDeVictoria)
+
+let soyUnNumeroDeVictoria = 26
+    console.log('numero', soyUnNumeroDeVictoria)
+
+let soyUnArrayDeVictoria = ['DinerDash', 'Sudoku', 'MarioKart']
+    console.log('array', soyUnArrayDeVictoria)
+
+let soyUnObjectDeVictoria = {
+    nombre: "Victoria",
+    apellido: "Schwint",
+}
+    console.log('object', soyUnObjectDeVictoria)
+
+function funcionDeVictoria () {
+    console.log(soyUnStringDeVictoria)}
+
+funcionDeVictoria()
