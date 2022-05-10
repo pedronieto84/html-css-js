1dedbb5f1f92126967d23609eef3322068e0a234
AxelWine <contacto.axelwine@gmail.com>
2022-05-10 10:46:47 +0200

    He creado un string, object, array, number y una función

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ console.log(soyUnBooleanoDeAlex);
 
 let soyUnUndefinedDeAlex = undefined;
 console.log(soyUnUndefinedDeAlex);
+
+let soyUnStringDeAlex = "hola mundo";
+console.log(soyUnStringDeAlex);
+
+let soyUnObjectDeAlex = {
+    nombre: "Alexander",
+    cuellar: "Aguilar"
+};
+console.log(soyUnObjectDeAlex);
+
+let soyUnArrayDeAlex = ["Discord","Steam","OperaGX"];
+console.log(soyUnArrayDeAlex);
+
+let soyUnNumber = 2022;
+console.log(soyUnNumber);
+
+function soyUnaFuncionDeAlex(string) {
+    console.log(string);
+};
