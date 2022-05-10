b2db431a459b89a498dedd1a0b96674216f2716c
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-10 11:32:54 +0200

    javascript se ejecuta de forma lineal, de arriba a abajo (Hay excepciones, las veremos más adelante, que son las Promises).

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// FORMAS DE DEFINIR VARIABLES
-
-// LeT es para cuando esa variable puede cambiarse y solo afecta al scope de esa función
+// Por ahora, javascript se ejecuta de forma lineal, de hacia arriba hacia abajo, linea a linea.
 let nombre = 'Pedro'
+console.log(nombre)
 
-// El const es para una variable que no se puede cambiar
-const dni = '234234324x'
+nombre = 'Luis'
+console.log(nombre)
 
-// El Var es para una variable que siempre se define en el scope global CONSEJO NO USARLA CASI NUNCA !!!!!!!
-var localidad = 'Terrassa'
 
