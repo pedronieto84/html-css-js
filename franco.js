9d371f86a1bb521850fb031dfb153f5280ddc9e0
FrancOlano <olanofranco1@gmail.com>
2022-05-10 10:27:57 +0200

    booleano null y undefined de franco

---
+++ franco.js
@@ -0,0 +1,8 @@
+let soyElNullDeFranco = null
+console.log(soyElNullDeFranco)
+
+let soyElBooleanoDeFranco = true
+console.log(soyElBooleanoDeFranco)
+
+let soyElUndefinedDeFranco = undefined
+console.log(soyElUndefinedDeFranco)
