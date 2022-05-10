5e46dab8f4dc9b1989585510758056b7625c85b0
Mov1ux <javimourelo@hotmail.com>
2022-05-10 10:33:53 +0200

    Null,booleano,undefine

---
+++ index.js
@@ -0,0 +1,8 @@
+let soyElNullDeJavi = null 
+console.log("null", soyElNullDeJavi)
+
+let soyElBooleanoDeJavi = false
+console.log("booleano", soyElBooleanoDeJavi) 
+
+let soyElUndefineDeJavi = undefined
+console.log("undefined", soyElUndefineDeJavi)
