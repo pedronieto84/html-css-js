b29f9be1168f79910689a173ec1556f7a8786917
abrilramosa <abrilramosa@gmail.com>
2022-05-10 10:27:53 +0200

    Null, booleano, undefined

---
+++ index.js
@@ -0,0 +1,8 @@
+let soyElNulldeAbril = null
+console.log ('null', null)
+
+let soyelBooleanodeAbril = true
+console.log ('booleano', booleano)
+
+let soyElUndefinedDeAbril = 'Hola'
+console.log ('undefined', undefined)
