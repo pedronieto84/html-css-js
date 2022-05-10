26ac72cd6da7a1fbb51ec93c649bac6c2a36ca57
VicJGR <vjgomezrios@gmail.com>
2022-05-10 11:03:50 +0200

    Array, number, Object, Funcion, string VJ

--- Victor.js
+++ Victor.js
@@ -6,3 +6,23 @@ console.log("undefined", elUndefinedDeVictor)
 
 let soyElBooleanoDeVictor = false
 console.log("false", soyElBooleanoDeVictor)
+
+let elStringDeVictor = 'Hola'
+console.log("Hola", elStringDeVictor)
+
+let elNumeroDeVictor = 9
+console.log("numero", elNumeroDeVictor)
+
+let soyElArrayDeVictor =["Hitman", "Monster Hunter", "Pokemon"]
+console.log("array", soyElArrayDeVictor)
+
+let soyElObjectDeVictor = {
+    soyElApellido:"Gómez",
+    soyElNombre: "Joel"
+}
+console.log("object", soyElObjectDeVictor)
+
+function soyLaFuncion(){
+    console.log(elStringDeVictor)
+}
+soyLaFuncion()
