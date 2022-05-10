d912a60619189201f187238172aacac8b6e63afb
Adrix8 <adrianperezpascual@gmail.com>
2022-05-10 10:30:40 +0200

    Prueba de null, undefined y booleano

---
+++ adri.js
@@ -0,0 +1,8 @@
+let holaSoyUnNull = null
+console.log("null", holaSoyUnNull);
+
+let holaSoyUnUndefined = undefined
+console.log("undefined", holaSoyUnUndefined);
+
+let holaSoyUnBooleano = false
+console.log("Boleano", holaSoyUnBooleano );
