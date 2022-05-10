98fececdf9968102e617bba05e900fcc5bdeb425
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-10 11:36:10 +0200

    Las funciones se pueden definir donde se quiera.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-// Por ahora, javascript se ejecuta de forma lineal, de hacia arriba hacia abajo, linea a linea.
-let nombre = 'Pedro'
-console.log(nombre)
+// Las funciones se pueden definir donde se quiera, porque javascript primero hace un barrido de toda la pagina
+// Busca las funciones, las entiende y despues, hace un segundo barrido donde ya va linea a linea.
 
-nombre = 'Luis'
-console.log(nombre)
+let nombre = 'Pedro';
 
 
+
+
+loguearNombre()
+
+function loguearNombre() {
+  nombre = "Luis";
+  console.log(nombre);
+}
