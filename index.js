7a58196ac177ea57d6dc23ffbf199f5a5e0d3fb6
marcelhannouti <mel@elpuig.xeill.net>
2022-05-10 10:58:44 +0200

    funcion,object,string,numero ejemplo js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 let soyelNullmarc = null
 console.log(`null`,soyelNullmarc)
 
+
 let soyelbooleanomarc = true 
 console.log(`boolean`,soyelbooleanomarc)
 
 let soyelundefinedmarc
 console.log(`undifiend`,soyelundefinedmarc)
+
+let letsoyelstringmarc = marc
+console.log(`string`,letsoyelstringmarc)
+
+let soyelnumeromarc =3
+console.log(`numero`,soyelnumeromarc)
+
+let soyelarraymarc =[`archero`,`dokkan battle`,`dragon ball legends`]
+
+let soyelobjectmarc = {
+    nombre:marc,
+    apellido:elhannouti
+}
+
+function soylafunctionmarc(soyelarraymarc){
+    console.log(`array`,soyelarraymarc)
+}
+console.log(`funcion`,soylafunctionmarc)
