d07b6e2ed948142246b311ae3e3aa392b3ad81de
victoriaschwin <vicky.schwint95@gmail.com>
2022-05-10 10:33:21 +0200

    creado index.js y agregado un null, boolean y undefined

---
+++ index.js
@@ -0,0 +1,8 @@
+let soyElNullDeVictoria = null
+console.log('null', soyElNullDeVictoria)
+
+let soyUnUndefinedDeVictoria = undefined
+console.log('undefined', soyUnUndefinedDeVictoria)
+
+let soyUnBooleanoDeVictoria = false
+console.log('booleano', soyUnBooleanoDeVictoria)
