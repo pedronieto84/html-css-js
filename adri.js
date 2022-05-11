32c6a06cf7a7fa40cbd429188d07a5d75f98c852
Adrix8 <adrianperezpascual@gmail.com>
2022-05-11 10:56:25 +0200

    operaciones

--- adri.js
+++ adri.js
@@ -1,8 +1,28 @@
-edad = 70
-if (edad <= 18){
-    console.log("eres menor de edad")
-}else if (edad >= 65){
-    console.log("abuelo")
-}else{
-    console.log("edad para trabajar")
+const operador = "+"
+const numero1 = 2
+const numero2 = 3
+let resultado
+
+operacionMatematica(operador, numero1, numero2, resultado)
+function operacionMatematica (operador, numero1, numero2, resultado){
+    if (operador === "suma") {
+        resultado = numero1 + numero2
+        console.log(resultado)
+        return resultado;
+    }else if (operador === "resta") {
+        resultado = numero1 - numero2
+        console.log(resultado)
+        return resultado;
+    }else if (operador === "multiplicacion") {
+        resultado = numero1 * numero2
+        console.log(resultado)
+        return resultado;
+    }else if (operador === "dividision") {
+        resultado = numero1 / numero2
+        console.log(resultado)
+        return resultado;
+    }else {
+        return "ponme un operador valido tonto";
+    }
+
 }
