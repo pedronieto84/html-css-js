77b8bff63d9d4b6f035bdb6162d974dd8a4fb96d
AxelWine <contacto.axelwine@gmail.com>
2022-05-11 13:06:20 +0200

    email encontrado

--- index.js
+++ index.js
@@ -1,62 +1,20 @@
-
-//
-// EJERCICIO 1:
-// Función de operaciones matemáticas.
-//
-function operacionMatematica(operacion, numero1, numero2) {
-    switch (operacion) {
-        case "suma":
-            return numero1+numero2;
-        case "resta":
-            return numero1-numero2;
-        case "multiplicación":
-            return numero1*numero2;
-        case "división":
-            return numero1/numero2;
-        default:
-            return "Parametro erroneo";
-    }
-}
-
-// Ejemplo de uso:
-console.log("suma", operacionMatematica("suma", 15, 20));
-console.log("resta", operacionMatematica("resta", 10, 5));
-console.log("multiplicación", operacionMatematica("multiplicación", 20, 2));
-console.log("división", operacionMatematica("división", 4, 2));
-console.log("otro", operacionMatematica("otro", 4, 2));
-
-//
-// EJERCICIO 2:
-// Función para devolver una ciudad.
-//
-const ciudades = ["Barcelona","Madrid","Málaga","Valencia","La Coruña"];
-function devolverPosicionArray(array, posicion) {
-    return array[posicion];
+const objetoAnidadoComplejo = {
+    user: {
+      email: {
+        item: [
+          {
+            email: "adfasdfas",
+            password: "adfasdfad",
+            item: [
+              "adfadfasdf",
+              12312312,
+              "adsfasdfad",
+              ["adsfasdf", [{ email: "pedro.nieto.sanchez@gmail.com" }]],
+            ],
+          },
+        ],
+      },
+    },
 };
-console.log("Oteber ciudad 1", devolverPosicionArray(ciudades, 0));
-console.log("Oteber ciudad 3", devolverPosicionArray(ciudades, 2));
 
-//
-// EJERCICIO 3:
-// Función para obtener claves de todas las propiedades.
-//
-let perfil = {
-    nombre: "Alexander",
-    apellidos: "Cuellar Aguilar",
-    edad: 19,
-    ciudad: "Barcelona"
-}
-function obtenerClaves(objeto) {
-    let claves = [];
-    let valores = [];
-    for(let propiedad in objeto) {
-        claves.push(propiedad);
-        valores.push(objeto[propiedad]);
-    };
-
-    let resultado = [claves, valores];
-    return resultado;
-}
-
-// Ejemplo de uso:
-console.log(obtenerClaves(perfil));
+console.log(objetoAnidadoComplejo.user.email.item[0].item[3][1][0].email);
