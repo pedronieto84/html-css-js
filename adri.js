efa5deaa0454ddb4e259b599a21c2c6d73baf36f
Adrix8 <adrianperezpascual@gmail.com>
2022-05-11 12:57:43 +0200

    ejercicio 2 con while

--- adri.js
+++ adri.js
@@ -1,27 +1,20 @@
-vconst operador = "suma"
-const numero1 = 2
-const numero2 = 3
-let resultado
+/// Segundo Ejercicio
 
-operacionMatematica(operador, numero1, numero2, resultado)
-function operacionMatematica (operador, numero1, numero2, resultado){
-    if (operador === "suma") {
-        resultado = numero1 + numero2
-        console.log(resultado)
-        return resultado;
-    }else if (operador === "resta") {
-        resultado = numero1 - numero2
-        console.log(resultado)
-        return resultado;
-    }else if (operador === "multiplicacion") {
-        resultado = numero1 * numero2
-        console.log(resultado)
-        return resultado;
-    }else if (operador === "dividision") {
-        resultado = numero1 / numero2
-        console.log(resultado)
-        return resultado;
-    }else {
-        return "ponme un operador valido tonto";
+// Crear un array con 5 ciudades (string).
+// Crear una función llamada devolver posicion del array que va a recibir los siguientes parametros
+
+
+// Primer parametro, el array de las ciudades
+// Segundo parametro, el indice
+
+// Me deve devolver el nombre de la ciudad que esta en esa posicion.
+const arrayCiudades = ["San Francisco", "Memphis", "Milwakee", "Boston"]
+let indice = 0
+devolverPosicion (arrayCiudades, indice)
+function devolverPosicion (arrayCiudades, indice){
+    while (indice<4) {
+        console.log(arrayCiudades[indice])
+        indice++
+        
     }
 }
