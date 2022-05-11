020f0d62bec5cce9c60eb67bcca2c5f93a6c8515
abrilramosa <abrilramosa@gmail.com>
2022-05-11 10:24:17 +0200

    primeraparteejercicionofinal

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-function mayoriaDeEdad (edad){
-
-    if (edad >=65){
-        console.log("tienes", edad,"años y","eres un jubilado")
-    }
-    else if (edad >= 18){
-        console.log ("tienes", edad,"años y","estas en edad de trabajar")
-    }
-    else {
-        console.log ("tienes", edad,"años y","eres menor de edad. ¡NO PUEDES ENTRAR!")
-    }
-
+function OperacionMatematica(){
+    suma, resta, multiplicación, división
+    numero1
+    numero2
 }
-
-mayoriaDeEdad (70)
-mayoriaDeEdad (35)
-mayoriaDeEdad (9)
