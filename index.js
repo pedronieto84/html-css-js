ef71b21ef26cb394ab43222c72ea5e29ee1bb620
Mov1ux <javimourelo@hotmail.com>
2022-05-11 11:01:41 +0200

    Ejercicio

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
-function mayoriaDeEdad (edad){
-    if (edad <= 18) {
-        console.log("es menor de edad")
-    } else if( edad >=65) {
-            console.log("es jubilado")
-    } else{
-        console.log("estas en  edad de trabajar")}
-    }
-mayoriaDeEdad ()
+// Primer ejercicio
+
+// 1 Crear una función que se llame operacionMatematica. Que va a recibir como parámetros,
+
+// Primer parametro: 'suma', 'resta', 'multiplicacion', 'division' 
+// Segundo parametro: numero
+// Tercer parametro: numero
+
+const operacion = ""
+console.log("suma", suma)
+
+const numero1 = 10 + suma
+console.log("Resultado suma", numero1)
+const numero2 = 5 * suma
+console.log("Resultado multi", numero2)
+
+
+
+
+
+
+// Me tiene que devolver el resultado de la operación que le he indicado.
+
+// Si le paso un string, que no coincide con ('suma', 'resta', 'multiplicacion', 'division') me deve devolver el texto "Parametro erroneo"
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
+
+//// Segundo Ejercicio
+
+// Crear un array con 5 ciudades (string).
+
+const arrayCiudades = ["bcn","Madrid","Sevilla","Paris","Tokyo",]
+
+// Crear una función llamada devolver posicion del array que va a recibir los siguientes parametros
+
+
+// Primer parametro, el array de las ciudades
+// Segundo parametro, el indice
+
+const devolverPosicionArray = arrayCiudades[0]
+console.log("Ciudad 1", devolverPosicionArray)
+
+const ciudad2 = arrayCiudades[1]
+console.log("Ciudad 2", ciudad2)
+
+const ciudad3 = arrayCiudades[2]
+console.log("ciudad 3", ciudad3)
+
+
+// Me deve devolver el nombre de la ciudad que esta en esa posicion.
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+//// Tercer Ejercicio
+
+// Definis un objeto usuario, que tenga email, password, y nombre. 
+// Y creamos una función que recibe como parámetros ese objeto.
+// Me tiene que devolver una array de strings con las claves de todas las propiedades.
