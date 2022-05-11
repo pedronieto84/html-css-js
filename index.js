4c71ef1bf0d8cbedfb9b01fbeb6f4a96545ed564
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-11 09:49:15 +0200

    Ejemplo de Ternario (forma simplificada de expresar un if else)

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// Juntar strings, o "string interpolation"
+// Ternario es una forma de expresar un if else, si puedo expresar el resultado del if y el else en una unica linea
 
-const nombre = "Pedro";
-const apellidoPaterno = "Nieto";
-const apellidoMaterno = "Sanchez";
+const edad = 21
 
-// Forma anterior
+const soyMayorDeEdad =  ( edad > 18 ) ? 'si' : 'no';
+console.log('soy mayor de edad', soyMayorDeEdad )
 
-const nombreCompleto = nombre + " " + apellidoPaterno + " " + apellidoMaterno;
-console.log("concatenar strings", nombreCompleto);
 
-// Forma nueva de concatenar stringa
-const nombreCompletoFormaNueva = `${nombre} ${apellidoPaterno} ${apellidoMaterno}`;
-console.log("concatenar strings forma moderna", nombreCompletoFormaNueva);
+const estoyEnEdadDeTrabajar = (  ( edad > 18 ) && (edad < 66 ) ) ? 'si' : 'no'
+console.log("estoy en edad de trabajar", estoyEnEdadDeTrabajar)
+
