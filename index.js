781e735559945fb953f64013a5f76b6b76058f80
AxelWine <contacto.axelwine@gmail.com>
2022-05-11 10:30:43 +0200

    He hecho el ejercicio 1 y el ejercicio 2

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 
-function mayorDeEdad(edad) {
-    if( edad >= 65 ) console.log("Es un jubilado");
-    else if( edad > 18 ) console.log("Es mayor de edad");
-    else console.log("Es menor de edad");
-};
+//
+// EJERCICIO 1:
+// Función de operaciones matemáticas.
+//
+function operacionMatematica(operacion, numero1, numero2) {
+    switch (operacion) {
+        case "suma":
+            return numero1+numero2;
+        case "resta":
+            return numero1-numero2;
+        case "multiplicación":
+            return numero1*numero2;
+        case "división":
+            return numero1/numero2;
+        default:
+            return;
+    }
+}
 
-mayorDeEdad(12);
-mayorDeEdad(20);
-mayorDeEdad(80);
+// Ejemplo de uso:
+console.log("suma", operacionMatematica("suma", 15, 20));
+console.log("resta", operacionMatematica("resta", 10, 5));
+console.log("multiplicación", operacionMatematica("multiplicación", 20, 2));
+console.log("división", operacionMatematica("división", 4, 2));
+
+//
+// EJERCICIO 2:
+// Función para devolver una ciudad.
+//
+const ciudades = ["Barcelona","Madrid","Málaga","Valencia","La Coruña"];
+function devolverPosicionArray(array, posicion) {
+    return array[posicion];
+}
