ad1c560cf6ccb45a396d0c4d80d3a69f8241b4e0
vazquezpmarcos <vazquezpmarcos@gmail.com>
2022-05-11 12:21:37 +0200

    == -> === fix

--- index.js
+++ index.js
@@ -11,16 +11,16 @@
 // Si le paso un string, que no coincide con ('suma', 'resta', 'multiplicacion', 'division') me deve devolver el texto "Parametro erroneo"
 
 function operacionMatematica(operacion, num1, num2){
-    if (operacion == "suma"){
+    if (operacion === "suma"){
         return (num1 + num2);
     }
-    else if (operacion == "resta"){
+    else if (operacion === "resta"){
         return (num1 - num2);
     }
-    else if ((operacion == "multiplicacion") || ("multiplicación")){
+    else if ((operacion === "multiplicacion") || ("multiplicación")){
         return (num1 * num2);
     }
-    else if (operacion == "division"){
+    else if (operacion === "division"){
         return (num1 / num2);
     }
     else{
