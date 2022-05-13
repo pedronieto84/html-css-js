c0394629283cea2ada3781cf54e52f7467de4e06
AxelWine <contacto.axelwine@gmail.com>
2022-05-13 11:22:24 +0200

    He instalado express y he creado el gitignore

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const objetoAnidadoComplejo = {
-    user: {
-      email: {
-        item: [
-          {
-            email: "adfasdfas",
-            password: "adfasdfad",
-            item: [
-              "adfadfasdf",
-              12312312,
-              "adsfasdfad",
-              ["adsfasdf", [{ email: "pedro.nieto.sanchez@gmail.com" }]],
-            ],
-          },
-        ],
-      },
-    },
-};
+let hola = "ok";
 
-console.log(objetoAnidadoComplejo.user.email.item[0].item[3][1][0].email);
+function test() {
+  let hola = "test";
+
+  console.log(hola);
+  console.log(globalThis.hola);
+}
+
+test();
