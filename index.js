fd2caefcc0a77f6f5248b1433e7adc06a813c0c0
marcelhannouti <mel@elpuig.xeill.net>
2022-05-13 10:30:14 +0200

    item

--- index.js
+++ index.js
@@ -25,3 +25,27 @@ function soylafunctionmarc(soyelarraymarc){
     console.log(`array`,soyelarraymarc)
 }
 console.log(`funcion`,soylafunctionmarc)
+
+
+const objetoAnidadoComplejo = {
+    user: {
+      email: {
+        item: [
+          {
+            email: "adfasdfas",
+            password: "adfasdfad",
+            item: [
+              "adfadfasdf",
+              12312312,
+              "adsfasdfad",
+              ["adsfasdf", [{ email: "pedro.nieto.sanchez@gmail.com" }]],
+            ],
+          },
+        ],
+      },
+    },
+  };
+  
+  const accederAlItem = objetoAnidadoComplejo.user.email.item[0].item[3][1][0].email
+  console.log(accederAlItem)
+  
