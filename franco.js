5ec5b2e5600cbc5c04d0492f4954f4bc65a4e76e
FrancOlano <olanofranco1@gmail.com>
2022-05-13 12:23:38 +0200

    comit app.get dia de la semana

--- franco.js
+++ franco.js
@@ -1,24 +1,32 @@
-const objetoAnidadoComplejo = {
-    user: {
-      email: {
-        item: [
-          {
-            email: "adfasdfas",
-            password: "adfasdfad",
-            item: [
-              "adfadfasdf",
-              12312312,
-              "adsfasdfad",
-              ["adsfasdf", [{ email: "pedro.nieto.sanchez@gmail.com" }]],
-            ],
-          },
-        ],
-      },
-    },
-  };
+const express = require('express')
+const app = express()
+const port = 3000
 
-  const accederAlObjeto = objetoAnidadoComplejo.user.email.item[0].item[3][1][0].email
+app.get('/lunes', (req, res) => {
+  res.send('Hoy es lunes <br> <a href="/martes">siguiente</a>')
+})
+
+app.get('/martes', (req, res) => {
+  res.send('Hoy es martes <br> <a href="/miercoles">siguiente</a>')
+})
+app.get('/miercoles', (req, res) => {
+  res.send('Hoy es miercoles <br> <a href="/jueves">siguiente</a>')
+})
+app.get('/jueves', (req, res) => {
+  res.send('Hoy es jueves <br> <a href="/viernes">siguiente</a>')
+})
+app.get('/viernes', (req, res) => {
+  res.send('Hoy es viernes <br> <a href="/sabado">siguiente</a>')
+})
+app.get('/sabado', (req, res) => {
+  res.send('Hoy es sabado <br> <a href="/domingo">siguiente</a>')
+})
+app.get('/domingo', (req, res) => {
+  res.send('Hoy es domingo <br> <a href="/lunes">volver a lunes</a>')
+})
 
 
 
-  console.log(accederAlObjeto)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
