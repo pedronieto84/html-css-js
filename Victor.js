9a6f591df9c4cf8b030768e5aace9cf910df281b
VicJGR <vjgomezrios@gmail.com>
2022-05-13 12:24:22 +0200

    Servidor Dias de la semana VJ

--- Victor.js
+++ Victor.js
@@ -1,44 +1,32 @@
-// Primer ejercicio
+const express = require('express')
+const app = express()
+const port = 3000
 
-// 1 Crear una función que se llame operacionMatematica. Que va a recibir como parámetros,
+app.get('/lunes', (req, res) => {
+  res.send('<h1>Lunes</h1> <br> <a href="/martes">siguiente</a>')
+})
+app.get('/martes', (req, res) => {
+    res.send('<h1>Martes</h1> <br> <a href="/miercoles">siguiente</a>')
+  })
+  app.get('/miercoles', (req, res) => {
+    res.send('<h1>Miercoles</h1> <br> <a href="/jueves">siguiente</a>')
+  })
+  app.get('/jueves', (req, res) => {
+    res.send('<h1>Jueves</h1> <br> <a href="/viernes">siguiente</a>')
+  })
+  app.get('/viernes', (req, res) => {
+    res.send('<h1>Viernes</h1> <br> <a href="/sabado">siguiente</a>')
+  })
+  app.get('/sabado', (req, res) => {
+    res.send('<h1>Sabado</h1> <br> <a href="/domingo">siguiente</a>')
+  })
+  app.get('/domingo', (req, res) => {
+    res.send('<h1>Domingo</h1> <br> <a href="/lunes">siguiente</a>')
+  })
 
-// Primer parametro: 'suma', 'resta', 'multiplicacion', 'division' 
-// Segundo parametro: numero
-// Tercer parametro: numero
-
-// Me tiene que devolver el resultado de la operación que le he indicado.
-
-// Si le paso un string, que no coincide con ('suma', 'resta', 'multiplicacion', 'division') me deve devolver el texto "Parametro erroneo"
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
-////////////////////////////////////////////////////////////////////////////////////
 
-
-//// Segundo Ejercicio
-
-// Crear un array con 5 ciudades (string).
-// Crear una función llamada devolver posicion del array que va a recibir los siguientes parametros
-
-
-// Primer parametro, el array de las ciudades
-// Segundo parametro, el indice
-
-// Me deve devolver el nombre de la ciudad que esta en esa posicion.
-
-////////////////////////////////////////////////////////////////////////////////////////
-
-//// Tercer Ejercicio
-
-// Definis un objeto usuario, que tenga email, password, y nombre. 
-// Y creamos una función que recibe como parámetros ese objeto.
-// Me tiene que devolver una array de strings con las claves de todas las propiedades.
-
-const miUsuario = {
-    nombre:"Victor",
-    password:"Gomez",
-    email:"vjgomezrios@gmail.com",
-}
-let arr = Object.entries(miUsuario);
-console.log(miUsuario);
-    
-    
