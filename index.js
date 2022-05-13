3b44ef8426c3d69c17d133f1a6ba78028cfb962e
Maru <marianalopezeikilis>
2022-05-13 12:20:02 +0200

    Routing

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-const objetoAnidadoComplejo = {
-  user: {
-    email: {
-      item: [
-        {
-          email: "adfasdfas",
-          password: "adfasdfad",
-          item: [
-            "adfadfasdf",
-            12312312,
-            "adsfasdfad",
-            ["adsfasdf", [{ email: "pedro.nieto.sanchez@gmail.com" }]],
-          ],
-        },
-      ],
-    },
-  },
-};
-// const accederAlItem = objetoAnidadoComplejo.user.console.log(objetoAnidadoComplejo.user.email.item.item[3]);
-console.log(objetoAnidadoComplejo.user.email.item[0][0].item[3][1][0].email)
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('YAESTAELNODEMON NO NO ')
+})
+app.get('/lunes', (req, res) => {
+  res.send('Lunes')
+})
+
+app.get('/martes', (req, res) => {
+  res.send('Martes')
+})
+
+app.get('/miercoles', (req, res) => {
+  res.send('Miercoles')
+})
+app.get('/jueves', (req, res) => {
+  res.send('Jueves')
+})
+app.get('/viernes', (req, res) => {
+  res.send('Viernes')
+})
+app.get('/adios-mundo', (req, res) => {
+  res.send('Adios Mundo!!!')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
