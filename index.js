25df6831c72128bc784c53821979b45201d700a9
imparrable <victorbcnvv@gmail.com>
2022-05-13 13:39:00 +0200

    fin 13/05

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Qué pasa chavales, soy un hacker!')
+app.use(express.json())
+
+app.post('/alta-usuario', ( req, res ) => {
+  console.log('usuario', req.body)
+  res.send(req.body)
 })
-
-app.get('/adiosmundo', (req, res) => {
-    res.send('Adiós mundo cruel')
-  })
-
-  app.get('/lunes', (req, res) => {
-    res.send('lunes triste')
-  })
-  app.get('/martes', (req, res) => {
-    res.send('martesicooo')
-  })
-  app.get('/miercoles', (req, res) => {
-    res.send('miercolessss')
-  })
-  app.get('/jueves', (req, res) => {
-    res.send('juerneeesss')
-  })
-  app.get('/viernes', (req, res) => {
-    res.send('que ya es viernes locoo')
-  })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-;
+  console.log(`Example app listening on port ${port}`);
+});
