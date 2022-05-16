a5d7148d84efa0118fdd9612ef97432edc877fcf
marcelhannouti <mel@elpuig.xeill.net>
2022-05-16 11:43:30 +0200

    marc

--- index.js
+++ index.js
@@ -1,51 +1,15 @@
-let soyelNullmarc = null
-console.log(`null`,soyelNullmarc)
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json())
 
 
-let soyelbooleanomarc = true 
-console.log(`boolean`,soyelbooleanomarc)
+app.post('/alta-usuario', ( req, res ) => {
+  console.log('usuario', req.body)
+  res.send(req.body)
+})
 
-let soyelundefinedmarc
-console.log(`undifiend`,soyelundefinedmarc)
-
-let letsoyelstringmarc = marc
-console.log(`string`,letsoyelstringmarc)
-
-let soyelnumeromarc =3
-console.log(`numero`,soyelnumeromarc)
-
-let soyelarraymarc =[`archero`,`dokkan battle`,`dragon ball legends`]
-
-let soyelobjectmarc = {
-    nombre:marc,
-    apellido:elhannouti
-}
-
-function soylafunctionmarc(soyelarraymarc){
-    console.log(`array`,soyelarraymarc)
-}
-console.log(`funcion`,soylafunctionmarc)
-
-
-const objetoAnidadoComplejo = {
-    user: {
-      email: {
-        item: [
-          {
-            email: "adfasdfas",
-            password: "adfasdfad",
-            item: [
-              "adfadfasdf",
-              12312312,
-              "adsfasdfad",
-              ["adsfasdf", [{ email: "pedro.nieto.sanchez@gmail.com" }]],
-            ],
-          },
-        ],
-      },
-    },
-  };
-  
-  const accederAlItem = objetoAnidadoComplejo.user.email.item[0].item[3][1][0].email
-  console.log(accederAlItem)
-  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
