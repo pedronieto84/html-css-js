96bd018717410042549bb355fd3fb2ef77a2ebfd
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-17 10:14:49 +0200

    Para acceder al elemento de un array si conozco el indice.

--- index.js
+++ index.js
@@ -7,53 +7,7 @@ const arrayDeObjetos = [
     { nombre: 'Laura', edad: 26 }, 
     { nombre: 'Cristina', edad: 17}
 ]
-console.log(arrayDeObjetos)
 
-// Metodo PUSH sirve para añadir un elemento a un array en la última posición.
-const nuevoUsuario = {nombre: 'Abril', edad: 23}
-arrayDeObjetos.push(nuevoUsuario);
+// Para acceder a un elemento de un array utilizo esta sintaxis INdice siempre es 0
 
-// Metodo POP es para quitar el ultimo elemento de un array.
-arrayDeObjetos.pop()
-console.log(arrayDeObjetos)
-
-// Metodo UNSHIFT es como el push pero me lo mete al principio del array
-arrayDeObjetos.unshift({nombre: 'Alexander', edad: 19})
-console.log(arrayDeObjetos)
-
-// Metodo SHIFT es como el pop pero quita el primer elemento del array
-arrayDeObjetos.shift()
-console.log(arrayDeObjetos)
-
-// Metodo FOREACH, itera sobre todos los elementos del array.
-arrayDeObjetos.forEach((usuario, index) => {
-    console.log('usuario', index,  usuario)
-})
-
-// Metodo FILTER, filtra un array.
-const arrayFiltrado = arrayDeObjetos.filter((usuario, index )=>{
-    return usuario.edad > 18
-})
-console.log(arrayFiltrado)
-
-// Metodo MAP me sirve para mapear un array de objetos
-const arrayMapeado = arrayDeObjetos.map((usuario, index)=>{
-    return {
-        nombre: usuario.nombre,
-        edad: usuario.edad,
-        nombreEdad: `${usuario.nombre} - ${usuario.edad}` 
-    }
-})
-console.log(arrayMapeado)
-
-// Metodo FIND me sirve para encontrar un elemento en un array
-
-const usuarioLuis = arrayDeObjetos.find((usuario, index)=>{
-    return usuario.nombre === 'Luis'
-})
-console.log('Usuario Luis', usuarioLuis)
-
-
-// Revisar el findByIndex, some, includes, hasAny... Si quieres saber mas pues en el web3 schools javascript arrays 
-
-
+const usuarioLaura = arrayDeObjetos[2]
