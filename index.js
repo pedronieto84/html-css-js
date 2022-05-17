90e6a0c76b2b1ba1fda912a97a98dd140161e7ca
Mov1ux <javimourelo@hotmail.com>
2022-05-17 10:59:35 +0200

    Consumir promesa

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // Definir promesa
-const promesa = () => {
+const promesa = (num) => {
     return new Promise((resolve,reject) => {
         setTimeout(() => {
-            console.log("2 sec")
-            resolve(true)
+        
+        if (num >=18 ){
+                resolve("Es mayor de 18")
+        }else{
+            reject("error")
+        }
         },2000);
     })
 }
 // consumir promesa
-promesa().then((res)=>{
-    console.log("respuesta")
+promesa(18)
+.then((res)=>{
+    console.log(res)
+    return
+})
+.catch((rej)=>{
+    console.log(rej)
 })
