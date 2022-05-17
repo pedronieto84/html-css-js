1a5b9481fbc5e1815f072b68d8932257511aee71
abrilramosa <abrilramosa@gmail.com>
2022-05-17 09:04:04 +0000

    Sintaxis: definir promesa

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const express = require("express");
-const app = express();
-const port = 6000;
+// Definir una promesa
 
-app.use(express.json())
+const Promesa = (edad) => {
+  return new Promise(
+    (resolve, reject)=> {
+      setTimeout (
+        ()=>{
+          if (edad > 18) {
+            
+          }
+        }
+        
+        , 2000)
 
-app.post('/alta-usuario', ( req, res ) => {
-  console.log('usuario', req.body)
-  res.send(req.body)
+    } 
+  ) 
+
+  
+}
+
+// Consumir una promesa
+promesa(false)
+// EL then se ejecuta cuando en la definición de la promesa se llama al resolve()
+.then((res)=>{
+  console.log('respuesta de la promesa', res)
+  return 'hola'
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+.then((res)=>{
+  // Puedo concatenar varios then, y el return de un then, lo consume el otro recibiendolo como parametro
+  console.log('recibo del then anterior', res)
+})
+.catch((e)=>{
+  // Lo que sea erroneo, pasará por aquí
+  console.error('error que me devuelve la promesa', e)
+})
+.finally((res)=>{
+  // El finally se ejecuta siempre
+  console.log('finally')
+})
