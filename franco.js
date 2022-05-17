ba8d6ab2a19899dad614812cd44843dd46c2e007
FrancOlano <olanofranco1@gmail.com>
2022-05-17 11:44:11 +0200

    promesa

--- franco.js
+++ franco.js
@@ -1,32 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const promesa=(num)=>{
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      if (num<18){
+        reject ('entrada denegada')
+      } else{
+        resolve ('entrada permitida')
+      }
+    }, 2000
+      )
+    })
+  
+  }
 
-app.get('/lunes', (req, res) => {
-  res.send('Hoy es lunes <br> <a href="/martes">siguiente</a>')
+promesa(17).then((res)=>{
+  console.log(res)
 })
-
-app.get('/martes', (req, res) => {
-  res.send('Hoy es martes <br> <a href="/miercoles">siguiente</a>')
-})
-app.get('/miercoles', (req, res) => {
-  res.send('Hoy es miercoles <br> <a href="/jueves">siguiente</a>')
-})
-app.get('/jueves', (req, res) => {
-  res.send('Hoy es jueves <br> <a href="/viernes">siguiente</a>')
-})
-app.get('/viernes', (req, res) => {
-  res.send('Hoy es viernes <br> <a href="/sabado">siguiente</a>')
-})
-app.get('/sabado', (req, res) => {
-  res.send('Hoy es sabado <br> <a href="/domingo">siguiente</a>')
-})
-app.get('/domingo', (req, res) => {
-  res.send('Hoy es domingo <br> <a href="/lunes">volver a lunes</a>')
-})
-
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+.catch((reject)=>{
+  console.log(reject)
 })
