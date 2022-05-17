8339aa1807abd222bfc184f1493d9a8790a7ac26
VicJGR <vjgomezrios@gmail.com>
2022-05-17 11:41:35 +0200

    Funcion Promesa, setTimeout y consumir la promesa

--- Victor.js
+++ Victor.js
@@ -1,32 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/lunes', (req, res) => {
-  res.send('<h1>Lunes</h1> <br> <a href="/martes">siguiente</a>')
+const promesa = (num) => {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          if(num >= 18){
+              resolve("Mayor de edad")
+          }else{
+              reject()
+          }
+          
+      }, 3000)
+  })
+}
+promesa(35)
+.then((res)=>{
+  console.log(res)
 })
-app.get('/martes', (req, res) => {
-    res.send('<h1>Martes</h1> <br> <a href="/miercoles">siguiente</a>')
-  })
-  app.get('/miercoles', (req, res) => {
-    res.send('<h1>Miercoles</h1> <br> <a href="/jueves">siguiente</a>')
-  })
-  app.get('/jueves', (req, res) => {
-    res.send('<h1>Jueves</h1> <br> <a href="/viernes">siguiente</a>')
-  })
-  app.get('/viernes', (req, res) => {
-    res.send('<h1>Viernes</h1> <br> <a href="/sabado">siguiente</a>')
-  })
-  app.get('/sabado', (req, res) => {
-    res.send('<h1>Sabado</h1> <br> <a href="/domingo">siguiente</a>')
-  })
-  app.get('/domingo', (req, res) => {
-    res.send('<h1>Domingo</h1> <br> <a href="/lunes">siguiente</a>')
-  })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+.catch((e)=>{
+  console.log("Menor de Edad")
 })
-
-
-
