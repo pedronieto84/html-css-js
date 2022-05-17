3628a57eb7bdde00c409c085e5234944596b5bfb
victoriaschwin <vicky.schwint95@gmail.com>
2022-05-17 11:01:30 +0200

    defini promesa con parametros

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-/* definir una promesa 
-la promesa va a usar un set timeout de 2s
+/*
+
 la promesa va a recibir como parametro un numero
 si el numero es menor a 18la promesa se reject de lo contrario se resolve
 */
 
 
-const promise = () => {
-  return new Promise (()=>{
-    setTimeout(()=>{},2000)
+const promise = (number) => {
+  return new Promise ((resolve, reject)=>{
+    setTimeout(()=>{
+      console.log('2 segundos')
+      if (number > 18){
+        resolve ( 'usuario mayor de edad')
+      } else {
+        reject ('usuario menor de edad')
+      }
+    },2000)
   })
 }
+
