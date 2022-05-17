545d5c43b48c0f45ecd3db3bdb1d0f6072213636
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-17 09:38:42 +0200

    Syntaxis de funciones de javascript.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+// Diferentes syntaxis para expresar una función
+
+function loguearData(){
+    console.log('loguear data')
+}
+
+const loguearDatos = () => {
+    console.log('loguear data, con arrow functions')
+}
+
+(() => {
+    console.log('función sin nombre, o anonymous function')
+})()
+
+loguearData()
+loguearDatos()
+
