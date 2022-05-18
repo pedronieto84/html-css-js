d2a56bd148054672e7a6ac21c93fa3eda955a15b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-18 09:37:07 +0200

    Diferencia entre Javascript Plain Object y JSON.

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
+// Javascript OBJECT o PLAIN JAVASCRIPT OBJECT
 
-// Las promesas permiten que la ejecución del código sea más eficiente en términos de tiempo y me permite no tener que esperar a la resolución de
-// la logica anterior. 
+const usuario = {
+    name: 'Pedro',
+    edad: 38
+}
 
-mesa1().then(()=>{
-    // 1 
-    // cuando llegue el plato, les sirvo
-})
-mesa2().then(()=>{
-    // 5 segundos
-        // cuando llegue el plato de la 2 pues se lo sirvo
-})
 
-mesa3().then(()=>{
-    // 2 segundos
-    // cuando llegue el plato de la 2 pues se lo sirvo
-})
+// JSON o JAVASCRIPT OBJECT NOTATION 
 
-mesa4().then(()=>{
-    // 4 segundos
-    // cuando llegue el plato de la 2 pues se lo sirvo
-})
+const usuarioJSON = {
+    "name": "pedro",
+    "edad": 38
+}
+
+
+
+
 
 
 
