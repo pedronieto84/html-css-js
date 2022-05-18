089a8e8b4a2348128934bcb22cc2314338a88153
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-18 10:18:51 +0200

    Objeto Math que me permite realizar operaciones matemáticas.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// Numero
+// Objeto Math me permite utilizar una serie de metodos relativos a Matematicas.
 
-const edad = 38
+// Math.random me genera un numero aleatorio
+const random = Math.random()
+console.log('numero aleatorio', random)
 
-// Convertir un number a string
-const numberAString = edad.toString()
-console.log('number a string', numberAString)
+// Para redondear, el Math.round()
+const numero = 3.6
+const redondeo = Math.round(numero)
+console.log('redondeo ', redondeo)
 
-// Convertir un decimal a un entero
-const numberDecimal = 3.43424132412341341234132413241324132412341234132412341324
-const decimalToInt = numberDecimal.toFixed(0)
-console.log('numbero quitandole deimclaes', decimalToInt);
+// Para redondear al alza es el ceil, para redondear ala baja es el floor...
+
+
 
 
 
