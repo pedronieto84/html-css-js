ff8efd9c2fe48a57bbc4fac7b06c5d5951ff8c10
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-18 10:11:14 +0200

    Metodos con strings.

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-// Javascript OBJECT o PLAIN JAVASCRIPT OBJECT
+// Metodos mas habituales de Strings
 
-const usuario = {
-    name: 'Pedro',
-    edad: 38
-}
+const nombre = 'barna ligue 2022 hola hola hola adios'
 
-// Metodos mas habituales de la keyword OBJECT.
+// Convertir un string en un array que divide el string en función de un caracter.
+const arrayDeString = nombre.split(' ')
+console.log('arrayDeString')
+
+// Quiero saber la longitud de un string
+const stringCorto = 'hola'
+const lengthOfString = stringCorto.length
+console.log('length', lengthOfString) // Es base 1
+
+// Convertir a mayusculas
+const mayusculas = nombre.toUpperCase()
+console.log('todo a mayusculas', mayusculas)
+
+// Cortar un string y ponerle ...
+const string3 = 'Hola me gustan los perros, los gatos, me gusta el cine, los libros y la playa.'
+const cortarString = string3.substring(0, 50) + '...'
+console.log('cortar string', cortarString)
 
 
-// EL keys me sirve para loguear las "keys"
-const keys = Object.keys(usuario)
-console.log('keys', keys);
+// toLowerCase -> Minusculas de
 
-// El values me sirve para loguea los "valores"
-const values = Object.values(usuario)
-console.log('values', values)
+// loLocaleUpperCase -> La Primera en Mayusculas
 
+// indexOf -> Te dice donde esta una letra 
 
-
+// Hay muchos mas...
 
 
 
