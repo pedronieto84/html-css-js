96cc7b3ccfc5966118469005c6548e2b8a1f41ec
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-18 10:14:20 +0200

    Ejemplos de metodos con numbers.

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
-// Metodos mas habituales de Strings
+// Numero
 
-const nombre = 'barna ligue 2022 hola hola hola adios'
+const edad = 38
 
-// Convertir un string en un array que divide el string en función de un caracter.
-const arrayDeString = nombre.split(' ')
-console.log('arrayDeString')
+// Convertir un number a string
+const numberAString = edad.toString()
+console.log('number a string', numberAString)
 
-// Quiero saber la longitud de un string
-const stringCorto = 'hola'
-const lengthOfString = stringCorto.length
-console.log('length', lengthOfString) // Es base 1
-
-// Convertir a mayusculas
-const mayusculas = nombre.toUpperCase()
-console.log('todo a mayusculas', mayusculas)
-
-// Cortar un string y ponerle ...
-const string3 = 'Hola me gustan los perros, los gatos, me gusta el cine, los libros y la playa.'
-const cortarString = string3.substring(0, 50) + '...'
-console.log('cortar string', cortarString)
-
-
-// toLowerCase -> Minusculas de
-
-// loLocaleUpperCase -> La Primera en Mayusculas
-
-// indexOf -> Te dice donde esta una letra 
-
-// Hay muchos mas...
-
+// Convertir un decimal a un entero
+const numberDecimal = 3.43424132412341341234132413241324132412341234132412341324
+const decimalToInt = numberDecimal.toFixed(0)
+console.log('numbero quitandole deimclaes', decimalToInt);
 
 
 
