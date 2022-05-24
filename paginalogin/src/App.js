6dbbf4f1724498358cf73c02364957718d1ea14e
abrilramosa <abrilramosa@gmail.com>
2022-05-24 11:53:13 +0200

    Importar BrowserRouter y código App para enrutador

--- paginalogin/src/App.js
+++ paginalogin/src/App.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Link} from "react-router-dom";
+import  BrowserRouter as Router, Switch, Route, Link  from "react-router-dom";
 
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
 
-export default function App() {
-  return (
-    <><div>
-      <h1>Hola Mundo</h1>
-    </div><nav>
-        <Link to='/Menu'>Menu</Link>
-        <Link to='/Login'>Login</Link>
-        <Link to='/Pie'>Pie</Link>
-      </nav></>
+function App() 
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to="/sign-in">Zentica.io</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/sign-in">Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sign-up">Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component=Login />
+            <Route path="/sign-in" component=Login />
+            <Route path="/sign-up" component=SignUp />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
-}
+
+
+export default App;
