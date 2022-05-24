42eec42a19017f9b8763aacb8feafcfbbcebfe0a
abrilramosa <abrilramosa@gmail.com>
2022-05-24 11:56:37 +0200

    Plantilla de formulario de inicio de sesion Login.component

--- paginalogin/src/App.js
+++ paginalogin/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import  BrowserRouter as Router, Switch, Route, Link  from "react-router-dom";
+import  BrowserRouter = Router, Switch, Route, Link  from "react-router-dom";
 
 import Login from "./components/login.component";
 import SignUp from "./components/signup.component";
@@ -28,9 +28,9 @@ function App()
       <div className="auth-wrapper">
         <div className="auth-inner">
           <Switch>
-            <Route exact path='/' component=Login />
-            <Route path="/sign-in" component=Login />
-            <Route path="/sign-up" component=SignUp />
+            <Route exact path='/' component= Login />
+            <Route path="/sign-in" component= Login />
+            <Route path="/sign-up" component= SignUp />
           </Switch>
         </div>
       </div>
